c0bcfd256b56685fd239500d860b0fdd1f5d6bc7
John Reitano <jreitano@gmail.com>
2023-07-26 14:03:56 -0700

    temporary seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-return if Customer.any?
+c = Customer.last
+c.highlight_types.create! name: "Positive Reaction", color: "#green"
+c.highlight_types.create! name: "Insight", color: "#blue"
+c.highlight_types.create! name: "Action Item", color: "#white"
 
-oppy = Customer.create! name: 'J. Robert Oppenheimer'
-oppy.highlight_types.create! name: 'Positive Reaction', color: '#green'
-oppy.highlight_types.create! name: 'Insight', color: '#blue'
-oppy.highlight_types.create! name: 'Action Item', color: '#white'
+# return if Customer.any?
 
-Customer.create! name: 'Werner Heisenberg'
+# c = Customer.create! name: "J. Robert Oppenheimer"
+# c.highlight_types.create! name: "Positive Reaction", color: "#green"
+# c.highlight_types.create! name: "Insight", color: "#blue"
+# c.highlight_types.create! name: "Action Item", color: "#white"
+
+# c = Customer.create! name: "Werner Heisenberg"
+# c.highlight_types.create! name: "Positive Reaction", color: "#green"
+# c.highlight_types.create! name: "Insight", color: "#blue"
+# c.highlight_types.create! name: "Action Item", color: "#white"
